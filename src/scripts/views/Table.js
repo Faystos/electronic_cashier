f4946daef16c22e631340e88b2eef466a42b88bc
Vladimir <fayst_@bk.ru>
2020-12-29 23:03:35 +0300

    Реализовал вывод таблицы с загруженными данными.

--- src/scripts/views/Table.js
+++ src/scripts/views/Table.js
@@ -6,60 +6,78 @@ export default class Table {
   
 
   init = arr => {
-    // console.log(arr);
-
-    const mainTable = this.createMainTable(arr);
-    console.log(mainTable);
-    
-    
-    
+    const mainTable = this.createMainTable(arr);    
     this.body.appendChild(mainTable);   
   }
 
   createMainTable = (arr) => {    
     const table = document.createElement('table');
+    table.classList.add('main_table');
     const tHead = this.createTHead();  
-    const tBody = this.createTBody(arr);
-    // console.log(tHead);
-    console.log(tBody);
+    const tBody = this.createTBody(arr);    
     table.insertAdjacentHTML('afterbegin', tHead);
-    table.insertAdjacentHTML('beforeend', tBody);
-    
+    table.insertAdjacentHTML('beforeend', tBody);    
     return table;
   }
 
   createTHead = () => {
-    const tHead =
-    `<thead>
-      <tr>
-        <td>имя</td>
-        <td>посты</td>
-        <td>кол-во коментариев</td>
-      </tr>
-    </thead>`;
+    const tHead = `
+      <thead>
+        <tr>
+          <td class='main_td'>Имя(автора)</td>
+          <td class='main_td'>Название(поста)</td>
+          <td class='main_td'>Количество(комментариев)</td>
+        </tr>
+      </thead>
+    `;
     return tHead;
   }
 
   createTBody = (arr) => {
+    const tName = arr.map(el => {     
+      const { post } = el;      
+     
+      const tPost = post.map(post => {
+        return `
+        <tr>
+          <td>Пост: ${post.title}</td>
+        </tr>  
+        `;
+      }).join(' ');
 
-    const tName = arr.map(el => {
+      const tComment = post.map(post => {
+        return `
+        <tr>
+          <td>${post.comment.length}</td>
+        </tr>  
+        `;
+      }).join(' ');
+
+
       return`
         <tr>
-          <td>${el.name}</td>      
+          <td class='main_td'>${el.name}</td>
+          <td class='main_td'>
+            <table>
+              ${tPost}      
+            </table>
+          </td>
+          <td class='main_td'>
+            <table>
+              ${tComment}      
+            </table>
+          </td>
         </tr>
       `;
     }).join(' ');
     
 
     const tBody = `
-    <tbody>
-      ${tName}
-    </tbody>    
+      <tbody>
+        ${tName}
+      </tbody>    
     `;
 
     return tBody;
-
   };
-
-
 }
